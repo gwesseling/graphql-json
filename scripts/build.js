390c9e90fcc2c16babfe108ad7a823595f3c9ebd
Gerard Wesseling <gerardwesseling16@gmail.com>
2024-05-03 22:56:59 +0200

    build(package): generate binary packages

--- scripts/build.js
+++ scripts/build.js
@@ -2,31 +2,37 @@
 const path = require("path");
 const fs = require("fs");
 
-const os = process.argv[2];
-const arch = process.argv[3];
+const OS = process.argv[2];
+const ARCH = process.argv[3];
+const LINUX = ["linux", "darwin"];
 
-let packageOS = os;
-let packageArch = arch;
+let packageOS = OS;
+let packageArch = ARCH;
 
-if (os === "windows") packageOS = "win32";
-if (arch === "amd64") packageArch = "x64";
-if (arch === 386) packageArch = "ia32";
+if (OS === "windows") packageOS = "win32";
+if (ARCH === "amd64") packageArch = "x64";
+if (ARCH === 386) packageArch = "ia32";
 
 const rootDir = path.dirname(__dirname);
 const npmDir = path.join(rootDir, "npm-binaries");
-const packageDir = path.join(npmDir, `${os}-${arch}`);
+const packageDir = path.join(npmDir, `${OS}-${ARCH}`);
 
 try {
+    if (LINUX.includes(OS)) {
+        fs.mkdirSync(path.join(packageDir, "bin"));
+        fs.renameSync(path.join(packageDir, "graphql-codegen"), path.join(packageDir, "bin", "graphql-codegen"));
+    }
+
     const packageJson = fs.readFileSync(path.join(npmDir, "package.json"), "utf8");
     const json = JSON.parse(packageJson);
 
     json.name = json.name.replace("$OS", packageOS).replace("$ARCH", packageArch);
-    json.description = json.description.replace("$OS", os).replace("$ARCH", arch);
+    json.description = json.description.replace("$OS", OS).replace("$ARCH", ARCH);
     json.os = [packageOS];
     json.cpu = [packageArch];
     json.private = false;
 
-    const readme = fs.readFileSync(path.join(npmDir, "README.md"), "utf8").replace("$OS", os).replace("$ARCH", arch);
+    const readme = fs.readFileSync(path.join(npmDir, "README.md"), "utf8").replace("$OS", OS).replace("$ARCH", ARCH);
 
     fs.writeFileSync(path.join(packageDir, "package.json"), JSON.stringify(json));
     fs.writeFileSync(path.join(packageDir, "README.md"), readme);
