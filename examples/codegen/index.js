a93417e1ab9f29c6b3cac3b503d54d41cdc72b8e
Gerard Wesseling <gerardwesseling16@gmail.com>
2024-06-30 20:18:57 +0200

    docs(codegen): update example

--- examples/codegen/index.js
+++ examples/codegen/index.js
@@ -1,5 +1,8 @@
 const {codegen} = require("@gwesseling/graphql-codegen");
 
-codegen(["input.json"])
+codegen({
+    inputFile: "input.json",
+    outputFile: "schema.ts",
+})
     .then((result) => console.log(result))
     .catch((err) => console.log(err));
--- examples/codegen/schema.ts
+++ examples/codegen/schema.ts
@@ -1,16 +1,39 @@
 import {
     GraphQLString,
-    GraphQLFloat,
-    GraphQLInputObjectType,
-    GraphQLEnumType,
-    GraphQLNonNull,
+    GraphQLInt,
+    GraphQLInterfaceType,
+    GraphQLID,
     GraphQLObjectType,
     GraphQLUnionType,
-    GraphQLInterfaceType,
-    GraphQLInt,
-    GraphQLID,
     GraphQLList,
+    GraphQLInputObjectType,
+    GraphQLFloat,
+    GraphQLNonNull,
+    GraphQLEnumType,
 } from "graphql";
+
+export const genre = new GraphQLEnumType({
+    name: "genre",
+    description: "The genre of the media item",
+    values: {
+        fantasy: {description: "Fantasy", value: "Fantasy"},
+        action: {description: "Action", value: "Action"},
+        comedy: {description: "Comedy", value: "Comedy"},
+        horror: {description: "Horror", value: "Horror"},
+    },
+});
+
+export const mediaItemInput = new GraphQLInputObjectType({
+    name: "mediaItemInput",
+    description: "Input for a media item",
+    fields: () => ({
+        title: {type: new GraphQLNonNull(GraphQLString)},
+        description: {type: GraphQLString},
+        type: {type: new GraphQLNonNull(GraphQLInt)},
+        price: {type: new GraphQLNonNull(GraphQLFloat)},
+        tags: {type: new GraphQLList(GraphQLString)},
+    }),
+});
 
 export const media = new GraphQLInterfaceType({
     name: "media",
@@ -30,13 +53,13 @@ export const movie = new GraphQLObjectType({
     description: "A movie",
     interfaces: [media],
     fields: () => ({
-        price: {type: new GraphQLNonNull(GraphQLFloat)},
         duration: {type: new GraphQLNonNull(GraphQLInt)},
         tags: {type: new GraphQLList(GraphQLString)},
         id: {type: new GraphQLNonNull(GraphQLID)},
         title: {type: new GraphQLNonNull(GraphQLString)},
         description: {type: GraphQLString},
         type: {type: new GraphQLNonNull(GraphQLInt)},
+        price: {type: new GraphQLNonNull(GraphQLFloat)},
     }),
 });
 
@@ -45,13 +68,13 @@ export const serie = new GraphQLObjectType({
     description: "A serie",
     interfaces: [media],
     fields: () => ({
-        seasons: {type: new GraphQLNonNull(GraphQLInt)},
-        tags: {type: new GraphQLList(GraphQLString)},
-        id: {type: new GraphQLNonNull(GraphQLID)},
         title: {type: new GraphQLNonNull(GraphQLString)},
         description: {type: GraphQLString},
         type: {type: new GraphQLNonNull(GraphQLInt)},
         price: {type: new GraphQLNonNull(GraphQLFloat)},
+        seasons: {type: new GraphQLNonNull(GraphQLInt)},
+        tags: {type: new GraphQLList(GraphQLString)},
+        id: {type: new GraphQLNonNull(GraphQLID)},
     }),
 });
 
@@ -65,26 +88,14 @@ export const query = new GraphQLObjectType({
     name: "query",
     description: "Queries",
     fields: () => ({
+        getMediaItems: {type: new GraphQLList(new GraphQLNonNull(mediaType)), description: "Get all media items"},
+        getMovies: {type: new GraphQLList(new GraphQLNonNull(movie)), description: "Get all movies"},
+        getSeries: {type: new GraphQLList(new GraphQLNonNull(serie)), description: "Get all series"},
         getMediaItem: {
             type: mediaType,
             description: "Get media item by id",
             args: {id: {type: new GraphQLNonNull(GraphQLID)}},
         },
-        getMediaItems: {type: new GraphQLList(new GraphQLNonNull(mediaType)), description: "Get all media items"},
-        getMovies: {type: new GraphQLList(new GraphQLNonNull(movie)), description: "Get all movies"},
-        getSeries: {type: new GraphQLList(new GraphQLNonNull(serie)), description: "Get all series"},
-    }),
-});
-
-export const mediaItemInput = new GraphQLInputObjectType({
-    name: "mediaItemInput",
-    description: "Input for a media item",
-    fields: () => ({
-        title: {type: new GraphQLNonNull(GraphQLString)},
-        description: {type: GraphQLString},
-        type: {type: new GraphQLNonNull(GraphQLInt)},
-        price: {type: new GraphQLNonNull(GraphQLFloat)},
-        tags: {type: new GraphQLList(GraphQLString)},
     }),
 });
 
@@ -98,15 +109,4 @@ export const mutation = new GraphQLObjectType({
             args: {mediaItem: {type: mediaItemInput}},
         },
     }),
-});
-
-export const genre = new GraphQLEnumType({
-    name: "genre",
-    description: "The genre of the media item",
-    values: {
-        horror: {description: "Horror", value: "Horror"},
-        fantasy: {description: "Fantasy", value: "Fantasy"},
-        action: {description: "Action", value: "Action"},
-        comedy: {description: "Comedy", value: "Comedy"},
-    },
 });
